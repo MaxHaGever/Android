596a50c6a85419ab715a742776f5563ef34bae36
MaxHaGever <maxspectorr@gmail.com>
2025-01-01 18:05:57 +0200

    Adding a new student UI

--- app/src/main/java/com/example/class1students/MainActivity.kt
+++ app/src/main/java/com/example/class1students/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.class1students
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentHostCallback
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +20,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        // TODO: 1 Button to navigate to add student
-        // TODO: 2 Create a button with a listener
-        //TODO: 3
+        // TODO: 1 Add student button
+        // TODO: 2 Navigate to add studnet
+        //TODO: 3 Create AddStudentLayout
+        //Todo: 4 Save student
+
+
+        val addStudentButton: Button = findViewById(R.id.main_activity_add_student_button)
+        addStudentButton.setOnClickListener {
+            val intent = Intent(this,AddStudentActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
